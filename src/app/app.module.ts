46bc558f63d74807aee34403a0e53c01a0a4ab87
Miguel-Parra <miguelts644@hotmail.com>
2022-04-25 22:35:38 -0500

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContadoresModule } from './contadores/contadores.module';
+import { HeroesModule } from './heroes/heroes.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HeroesModule,
+    ContadoresModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/heroes/heroe/heroe.component.ts
@@ -0,0 +1,37 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-heroe',
+    templateUrl: './heroe.component.html'
+})
+
+export class HeroeComponet {
+
+    nombre: string = "Ironman";
+    edad: number = 45;
+
+    get nombreCapitalizado(): string {
+        return this.nombre.toUpperCase();
+    }
+
+    obtenerNombre(): string {
+        return `${this.nombre} - ${this.edad}`
+    }
+
+    cambiarNombre(): void {
+        if(this.nombre === 'Ironman'){
+            this.nombre = 'Spiderman';
+        } else {
+            this.nombre = 'Ironman'
+        }
+    }
+
+    cambiarEdad(): void {
+        if(this.edad === 45){
+            this.edad = 18;
+        } else {
+            this.edad = 45;
+        }
+    }
+
+}
